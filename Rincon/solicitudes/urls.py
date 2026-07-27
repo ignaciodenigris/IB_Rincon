e5c5fee1bff1423ba418c1efb9a44449e50de94d
ignaciodenigris <166459983+ignaciodenigris@users.noreply.github.com>
2026-07-27 13:51:34 -0300

    Agrego rutas para aceptar y rechazar solicitudes

--- Rincon/solicitudes/urls.py
+++ Rincon/solicitudes/urls.py
@@ -1,13 +1,42 @@
-from django.urls import include, path
-from django.contrib import admin
+from django.urls import path
+
 from . import views
 
 
 urlpatterns = [
-    path("crear/<int:proyecto_id>/",views.crear_solicitud,name="crear_solicitud" ),
-    path("<int:solicitud_id>/",views.detalle_solicitud,name="detalle_solicitud"),
-    path("proveedor/",views.solicitudes_proveedor, name="solicitudes_proveedor"),
-    path("<int:solicitud_id>/confirmar/",views.confirmar_solicitud,name="confirmar_solicitud"),
-    path("<int:solicitud_id>/cancelar/",views.cancelar_solicitud,name="cancelar_solicitud"), 
+    path(
+        "crear/<int:proyecto_id>/",
+        views.crear_solicitud,
+        name="crear_solicitud",
+    ),
+    path(
+        "proveedor/",
+        views.solicitudes_proveedor,
+        name="solicitudes_proveedor",
+    ),
+    path(
+        "<int:solicitud_id>/",
+        views.detalle_solicitud,
+        name="detalle_solicitud",
+    ),
+    path(
+        "<int:solicitud_id>/aceptar/",
+        views.aceptar_solicitud,
+        name="aceptar_solicitud",
+    ),
+    path(
+        "<int:solicitud_id>/rechazar/",
+        views.rechazar_solicitud,
+        name="rechazar_solicitud",
+    ),
+    path(
+        "<int:solicitud_id>/confirmar/",
+        views.confirmar_solicitud,
+        name="confirmar_solicitud",
+    ),
+    path(
+        "<int:solicitud_id>/cancelar/",
+        views.cancelar_solicitud,
+        name="cancelar_solicitud",
+    ),
 ]
-
